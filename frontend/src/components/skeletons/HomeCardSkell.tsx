45d89af8f15079b0d3f1c98abfa769fb6ce1eaec
Samir Warsi <samirwarsi@Samirs-MacBook-Air.local>
2026-01-04 11:03:20 +0530

    miner change

--- frontend/src/components/skeletons/HomeCardSkell.tsx
+++ frontend/src/components/skeletons/HomeCardSkell.tsx
@@ -1,7 +1,7 @@
 export default function HomeCardSkell(){
     return (
         <div className="py-10 flex flex-col justify-center items-start md:grid md:grid-cols-2 xl:grid-cols-3 gap-5">
-            {[1,2,3,4,5,6].map((e,i)=>(<div key={i} className="h-80 w-72 rounded-md bg-slate-200"></div>))} 
+            {[1,2,3,4,5,6].map((i)=>(<div key={i} className="h-80 w-72 rounded-md bg-slate-200"></div>))} 
         </div>
     )
 }
